7a1bb777cd69e0ca554a6a9d6e9810021030bdd6
Jatin Patel <jatinpatel909192@gmail.com>
2015-08-07 12:48:57 +0530

    finished blog comments feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 class Article < ActiveRecord::Base
   
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def to_s
+  	name
+  end
+
+  def tag_list
+  	self.tags.collect do |tag|
+  		tag.name
+  	end.join(", ")
+  end
 end
